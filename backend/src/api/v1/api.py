984971fb6f9dd0e7ac103dadb624815ae6653131
VamshiS123 <114355089+VamshiS123@users.noreply.github.com>
2026-02-21 19:39:48 -0500

    phase 2

--- backend/src/api/v1/api.py
+++ backend/src/api/v1/api.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from src.api.v1.endpoints import farms
+
+from src.api.v1.endpoints import farms, builder
 
 api_router = APIRouter()
 api_router.include_router(farms.router, prefix="/farms", tags=["farms"])
+api_router.include_router(builder.router, prefix="/builder", tags=["builder"])
